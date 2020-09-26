2f80ff2a52e408a3d889069817d1f26ee96ad63e
Claus Rodrigues Tessmann <clausrt@outlook.com>
2020-09-26 16:26:13 -0300

    Segundo commit.... talvez.

---
+++ contexts/theme/ThemeContainer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { ColorModeProvider, CSSReset, ThemeProvider as ChakraThemeProvider } from '@chakra-ui/core';
+import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
+
+import customTheme from '../../styles/theme';
+
+const ThemeContainer: React.FC = ({ children }) => {
+    return (
+        <ChakraThemeProvider theme={customTheme}>
+            <ColorModeProvider value="dark">
+                <EmotionThemeProvider theme={customTheme}>
+                    <CSSReset/>
+                    {children}
+                </EmotionThemeProvider>
+            </ColorModeProvider>
+        </ChakraThemeProvider>
+    );
+};
+
+export default ThemeContainer;
